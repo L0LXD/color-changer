982b661de17c3eccd2d4c835af961d49ec2a6bd9
L0LXD <91483769+L0LXD@users.noreply.github.com>
2022-03-05 15:41:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function randomColor() {
+    return Math.floor(Math.random()*16777215).toString(16);}
+let color =  randomColor();
+
+const btn = document.getElementsById('bttn');
+console.log(btn);
